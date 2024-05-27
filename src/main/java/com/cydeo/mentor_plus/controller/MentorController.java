148e7ea52ee4261b8fa48884306e745a10d7d07a
Alim <koltikin210@gmail.com>
2024-05-27 21:23:51 +0300

    initial commit

--- src/main/java/com/cydeo/mentor_plus/controller/MentorController.java
+++ src/main/java/com/cydeo/mentor_plus/controller/MentorController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/mentors")
-@CrossOrigin
+@CrossOrigin()
 public class MentorController {
 
     private final MentorService mentorService;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,7 @@
 insert into mentors(like_count, name)
-values (2, 'Ower'),
-       (1, 'Matt'),
-       (1, 'Sahin');
+values (2, 'Yusuf'),
+       (5, 'Ziya'),
+       (15, 'Yasin'),
+       (20, 'Abdullah'),
+       (12, 'Ibrahim');
 
